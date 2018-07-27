c32b5a9bbfde22a4d78e5a6cfa487f28e1f49822
v.kulikov <v.kulikov@aimprosoft.com>
2018-07-27 16:27:52 +0300

    Changed def test suite

--- src/test/java/dockerTest/DefinitionTestSuite.java
+++ src/test/java/dockerTest/DefinitionTestSuite.java
@@ -2,11 +2,12 @@ package dockerTest;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 //@RunWith(CucumberWithSerenity.class)
 
-@RunWith(Cucumber.class)
+@RunWith(CucumberWithSerenity.class)
 @CucumberOptions(features="src/test/resources/GooleSearch.fetaure")
 public class DefinitionTestSuite {}
 
